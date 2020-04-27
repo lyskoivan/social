5a4bc9c050c00b6b86c7306f5ecdde217566cd06
Ivan <lysko1999i@gmail.com>
2020-04-27 23:42:49 +0300

    starter

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+import { port } from "./config";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("The sedulous hyena ate the antelope!");
+});
+
+app.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`server is listening on ${port}`);
+});
